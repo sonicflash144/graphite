c6bcb0fe55542019fe66515ce633ee3667737623
Sage Wang <pickmealeaf@gmail.com>
2024-10-02 07:10:42 -0500

    herokuurlupdate

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -78,7 +78,7 @@ function Chat({ editorContent, userPrompt, openSettings, clearStorage, onOpenThr
 
     localStorage.setItem('chatHistory', JSON.stringify(newChatHistory));
     try {
-      const response = await fetch("https://graphite-clone-0785836f9ec8.herokuapp.com/chat", {
+      const response = await fetch("https://graphite-clone-5fd9ae1385f0.herokuapp.com/chat", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ messages: newChatHistory }),
