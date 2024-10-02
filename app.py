4474994d58c65241dda1e295d637d1f44407cfcd
Sage Wang <pickmealeaf@gmail.com>
2024-10-02 06:39:52 -0500

    back

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-#load_dotenv('.env.local')
+load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
--- src/components/Thread.js
+++ src/components/Thread.js
@@ -92,7 +92,7 @@ function Thread({ editorContent, userPrompt, suggestion, onClose, onSuggestionHo
     localStorage.setItem('threadHistory', JSON.stringify(newThreadHistory));
 
     try {
-      const response = await fetch(process.env.HEROKU_URL, {
+      const response = await fetch("https://graphite-clone-5fd9ae1385f0.herokuapp.com/chat", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ messages: newThreadHistory }),
